941a60b2f7c344a31c651d9fcf986698ff0faa7f
David-2610 <davidtembhare1005@gmail.com>
2025-06-05 08:48:37 +0530

    BACKEND STARTED

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(PORT ,()=> {
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+})
