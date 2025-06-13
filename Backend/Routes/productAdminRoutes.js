612d9e110189d748b991949ae3f73eb60f92e7e5
David-2610 <davidtembhare1005@gmail.com>
2025-06-13 22:04:37 +0530

    New verse ALL DONE ENDS

--- Backend/Routes/productAdminRoutes.js
+++ Backend/Routes/productAdminRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const Product = require("../Models/Product");
 const {protect, admin} = require("../middleware/authmiddleware");
 
@@ -14,6 +15,61 @@ router.get("/", protect, admin, async (req, res) => {
   } catch (error) {
     console.error("Error fetching products:", error);
     res.status(500).json({ message: "Server error" });
+  }
+});
+// @route   POST /api/admin/products/addproduct
+// @desc    Add a new product (admin only)
+// @access  Private/Admin
+// POST /api/admin/products/addproduct
+router.post('/addproduct', protect, admin, async (req, res) => {
+  const userId = req.user._id; // Get the user ID from the authenticated user
+  try {
+
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      sku,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      rating,
+      numReviews
+    } = req.body;
+
+    const newProduct = new Product({
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      sku,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      rating,
+      numReviews,
+      user: userId 
+    });
+
+    
+
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 module.exports = router;
